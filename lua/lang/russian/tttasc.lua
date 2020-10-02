88095a8980884ce7af3b4859bb9174d4a297379b
Blueberryy <igory.ygr200@gmail.com>
2020-10-02 20:27:33 +0500

    Create tttasc.lua

---
+++ lua/lang/russian/tttasc.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["item_a_second_chance"] = "Второй шанс"
+L["item_a_second_chance_desc"] = "Получите второй шанс, убивая других людей... Стоит ли оно того? Чёрт возьми, да!"
+L["item_a_second_chance_corpse_desc"] = "Осторожно! Он может возродиться!"
+
+L["item_a_second_chance_chance_change"] = "Ваш шанс сейчас {chance}%"
+L["item_a_second_chance_no_revive"] = "Вы не воскреснете!"
+L["item_a_second_chance_popup_title"] = "Второй шанс"
+L["item_a_second_chance_popup_subtitle"] = "Вы воскреснете!"
+L["item_a_second_chance_revive_keys"] = "Нажмите {keycorpse}, чтобы возродиться у вашего трупа. Нажмите {keyspawn}, чтобы возродиться на месте возрождения."
