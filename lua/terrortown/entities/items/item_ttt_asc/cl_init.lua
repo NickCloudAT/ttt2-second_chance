012abfa92e6265c193e9499519e146c68434f8b5
NickCloud <nicolasbeiglboeck@gmx.at>
2020-11-23 14:34:08 +0100

    Fixed default bind issue

--- lua/terrortown/entities/items/item_ttt_asc/cl_init.lua
+++ lua/terrortown/entities/items/item_ttt_asc/cl_init.lua
@@ -10,26 +10,25 @@ net.Receive("ttt_asc_chance_change", function()
   chat.PlaySound()
 end)
 
-net.Receive("ttt_asc_show_reason", function()
-  local corpseKey = bind.Find("ttt_asc_respawn_corpse") == KEY_NONE and "NONE" or string.upper(input.GetKeyName(bind.Find("ttt_asc_respawn_corpse")))
-  local spawnKey = bind.Find("ttt_asc_respawn_spawn") == KEY_NONE and "NONE"or string.upper(input.GetKeyName(bind.Find("ttt_asc_respawn_spawn")))
+hook.Add("Initialize", "TTT_ASC_KeyBinds", function()
+  net.Receive("ttt_asc_show_reason", function()
+    local corpseKey = bind.Find("ttt_asc_respawn_corpse") == KEY_NONE and "NONE" or string.upper(input.GetKeyName(bind.Find("ttt_asc_respawn_corpse")))
+    local spawnKey = bind.Find("ttt_asc_respawn_spawn") == KEY_NONE and "NONE"or string.upper(input.GetKeyName(bind.Find("ttt_asc_respawn_spawn")))
 
-  LocalPlayer():SetRevivalReason("item_a_second_chance_revive_keys", {keycorpse = corpseKey, keyspawn = spawnKey})
+    LocalPlayer():SetRevivalReason("item_a_second_chance_revive_keys", {keycorpse = corpseKey, keyspawn = spawnKey})
+  end)
+
+  bind.Register("ttt_asc_respawn_corpse", function()
+    if not LocalPlayer():TTT2NETGetBool("ttt_asc_respawning_allowkey", false) then return end
+    net.Start("ttt_asc_key_respawn")
+    net.WriteBool(true)
+    net.SendToServer()
+  end, nil, "Items", "ASC - Respawn at corpse", KEY_R)
+
+  bind.Register("ttt_asc_respawn_spawn", function()
+    if not LocalPlayer():TTT2NETGetBool("ttt_asc_respawning_allowkey", false) then return end
+    net.Start("ttt_asc_key_respawn")
+    net.WriteBool(false)
+    net.SendToServer()
+  end, nil, "Items", "ASC - Respawn at spawn", KEY_SPACE)
 end)
-
-bind.Register("ttt_asc_respawn_corpse", function()
-  if not LocalPlayer():TTT2NETGetBool("ttt_asc_respawning_allowkey", false) then return end
-  net.Start("ttt_asc_key_respawn")
-  net.WriteBool(true)
-  net.SendToServer()
-end)
-
-bind.Register("ttt_asc_respawn_spawn", function()
-  if not LocalPlayer():TTT2NETGetBool("ttt_asc_respawning_allowkey", false) then return end
-  net.Start("ttt_asc_key_respawn")
-  net.WriteBool(false)
-  net.SendToServer()
-end)
-
-bind.AddSettingsBinding("ttt_asc_respawn_corpse", "ASC - Respawn at corpse", "Items", KEY_R)
-bind.AddSettingsBinding("ttt_asc_respawn_spawn", "ASC - Respawn at spawn", "Items", KEY_SPACE)
